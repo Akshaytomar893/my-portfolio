1998d5ca8225da1ea014ce9191761c77857ec76d
Akshay <akshaytomar891@gmail.com>
2024-08-10 22:10:15 +0530

    Stop tracking node_modules

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,8 +5,11 @@ import Navbar from '../Navbar/Navbar'
 const Home = () => {
   return (
     <>
-      <MainScreen heading={"Hello"} subHeading={"Hello Part 2"}/>
-      <Navbar/>
+      <MainScreen heading={"Hello World"} subHeading={"Feats Achieved"}>
+        <p>fndhfb</p>
+        <hr/>
+        </MainScreen>
+      {/* <Navbar/> */}
     </>
   )
 }
--- src/components/MainScreen/MainScreen.js
+++ src/components/MainScreen/MainScreen.js
@@ -1,10 +1,34 @@
-import React from 'react'
-import "../../Styles/GlobalStyles.css"
+import React, { useEffect } from "react";
+import "../../Styles/GlobalStyles.css";
 
-const MainScreen = ({heading, subHeading}) => {
+const MainScreen = ({ heading, subHeading, children }) => {
+  useEffect(() => {
+    debugger;
+    const heading = document.getElementById("heading");
+    if (heading) {
+      const words = heading.textContent.split(" ");
+      const lastWord = words.pop();
+      heading.innerHTML = `${words.join(" ")} <span>${lastWord}</span>`;
+    }
+
+    // return () => {
+    //   second
+    // }
+  }, []);
+
   return (
-    <div className='mainScreen'>MainScreen</div>
-  )
-}
+    <>
+      <div className="mainScreen">
+        <div className="headingContainer">
+          <p className="heading" id="heading">
+            {heading}
+          </p>
+          <p className="subHeading">{subHeading}</p>
+        </div>
+        <div>{children}</div>
+      </div>
+    </>
+  );
+};
 
-export default MainScreen
+export default MainScreen;
