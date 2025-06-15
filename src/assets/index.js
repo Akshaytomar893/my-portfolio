bc26d51924b7bbe29ba488bbc4ac0acfff7039e9
Akshay <akshay@Akshays-MacBook-Pro-2.local>
2025-06-16 00:00:09 +0530

    iages added

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,65 @@
 import ProfileImage from "./profile.png";
 import ColorPallete from "./color_pallette.png";
 import VeersaLogo from "./VeersaLogo.png";
-export { ProfileImage, ColorPallete, VeersaLogo };
+import Azure from "./azure.png";
+import CrossPlatform from "./cross-paltform.png";
+import Css from "./css.jpeg";
+import Debug from "./debug.png";
+import DevTools from "./dev-tools.png";
+import Dsa from "./dsa.png";
+import Firebase from "./firebase.jpeg";
+import Gcp from "./gcp.png";
+import Git from "./git.png";
+import Html from "./html.png";
+import Java from "./java.png";
+import Javascript from "./javascript.jpeg";
+import Jquery from "./jquery.png";
+import Mui from "./mui.png";
+import Next from "./next.png";
+import Nx from "./nx.png";
+import Python from "./python.png";
+import ReactNative from "./react-native.png";
+import Redux from "./redux.jpg";
+import Responsive from "./responsive.jpg";
+import RestApi from "./rest-api.png";
+import Saga from "./saga.jpeg";
+import Sass from "./saga.jpeg";
+import Sql from "./sql.png";
+import Typescript from "./typescript.png";
+import Vscode from "./vscode.png";
+import Webpack from "./webpack.png";
+import React from "./react.png";
+
+export {
+  ProfileImage,
+  ColorPallete,
+  VeersaLogo,
+  Azure,
+  CrossPlatform,
+  Css,
+  Debug,
+  DevTools,
+  Dsa,
+  Firebase,
+  Gcp,
+  Git,
+  Html,
+  Java,
+  React,
+  Javascript,
+  Jquery,
+  Mui,
+  Next,
+  Nx,
+  Python,
+  ReactNative,
+  Redux,
+  Responsive,
+  RestApi,
+  Saga,
+  Sass,
+  Sql,
+  Typescript,
+  Vscode,
+  Webpack,
+};
